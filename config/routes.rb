f20b70c8e22334ce4c30458e80a4f2cf01f78e1f
Pablo Torres <pablo.torres@insight-centre.org>
2016-03-11 13:38:57 +0000

    remove public signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
