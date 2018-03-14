396d04598b8d0991f7b1cae78631d647f3631ef7
Pablo Torres <pablo.torres@insight-centre.org>
2018-03-14 15:27:34 -0300

    adjust foods

--- db/gen_alimentos.sh
+++ db/gen_alimentos.sh
@@ -3,13 +3,15 @@
 cat ./alimentos.csv | awk -F "," '
 {if (substr($0,1,1) !~ /^#/) {
 	id = $1
-	sub(/[[:space:]]/,"_", id)
-  sub(/\//,"_", id)
-	sub(/á/,"a", id)
-	sub(/é/,"e", id)
-	sub(/í/,"i", id)
-	sub(/ó/,"o", id)
-	sub(/ú/,"u", id)
+	gsub(/[[:space:]]/,"_", id)
+  gsub(/\//,"_", id)
+	gsub(/á/,"a", id)
+	gsub(/é/,"e", id)
+	gsub(/í/,"i", id)
+	gsub(/ó/,"o", id)
+	gsub(/ú/,"u", id)
+	gsub(/ñ/,"n", id)
+	id = tolower(id)
 	if ($7 == "") {
 		print id" = Food.create!(name:\""$1"\",proteins:"$3",carbohydrates:"$4",fibre:"$5",lipids:"$6",saturated_fat:0)"
 	}
@@ -17,4 +19,4 @@ cat ./alimentos.csv | awk -F "," '
 		print id" = Food.create!(name:\""$1"\",proteins:"$3",carbohydrates:"$4",fibre:"$5",lipids:"$6",saturated_fat:0,price:"$7")"
 	}
 } 
-}'
+}' | sort -u
