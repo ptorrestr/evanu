6a0f9e59706770ac0cc668d00ae17b7b82f59215
Pablo Torres <pablo.torres@insight-centre.org>
2018-03-14 13:39:32 -0300

    simple food validation

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,8 @@
 class Food < ApplicationRecord
   belongs_to :user_id
+	validates :name, presence: true, uniqueness: true
+	validates :proteins, presence: true
+	validates :carbohydrates, presence: true
+	validates :fibre, presence: true
+	validates :lipids, presence: true
 end
