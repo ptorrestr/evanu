dbc6d5c6b0aa4c29b1bed98ebcc1a8848246b0dd
Pablo Torres <pablo.torres@insight-centre.org>
2018-03-16 15:23:52 -0300

    add 'pescado' for both levels. Add missing foods

--- db/gen_serving.sh
+++ db/gen_serving.sh
@@ -3,6 +3,7 @@
 cat once-medio-menor.csv once-prekinder.csv \
 	postre-medio-menor.csv postre-prekinder.csv \
 	pollo-medio-menor.csv pollo-prekinder.csv \
-	vacuno-medio-menor.csv \
+	vacuno-medio-menor.csv vacuno-prekinder.csv \
+	pescado-medio-menor.csv pescado-prekinder.csv \
 	| ./parse_serving.sh \
 	| sort -u
