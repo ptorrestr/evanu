02274d739a723e191601253ef1cfe4cde04ad470
Pablo Torres <pablo.torres@insight-centre.org>
2018-03-16 15:35:30 -0300

    add 'plato-fondo' for both categories

--- db/gen_serving.sh
+++ db/gen_serving.sh
@@ -5,5 +5,6 @@ cat once-medio-menor.csv once-prekinder.csv \
 	pollo-medio-menor.csv pollo-prekinder.csv \
 	vacuno-medio-menor.csv vacuno-prekinder.csv \
 	pescado-medio-menor.csv pescado-prekinder.csv \
+	plato-fondo-medio-menor.csv plato-fondo-prekinder.csv \
 	| ./parse_serving.sh \
 	| sort -u
